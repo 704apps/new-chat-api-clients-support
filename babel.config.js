2fb81405ba538c5ed3ec5310dfac8129d5d4bac1
DaniloSVJ <danilocotatoceu@gmail.com>
2024-08-09 16:06:55 -0300

    ajuste

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     presets: [
-      ["@babel/preset-env", { targets: { node: "current" } }],
-      "@babel/preset-typescript",
+      [
+      "@babel/preset-env",       "@babel/preset-typescript",
+      { targets: { node: "current" } }],
     ],
     plugins: [
       [
