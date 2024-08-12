d37210778e1c3833e01945f3a8dd2d9854e22b40
Joao <canalluka8760@gmail.com>
2024-08-12 18:33:08 -0300

    chore babel config file extensions

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-    presets: [
-      [
-      "@babel/preset-env",       "@babel/preset-typescript",
-      { targets: { node: "current" } }],
-    ],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./src/"],
-          alias: {
-            "@modules": "./src/modules",
-            "@main": "./src/main",
-            "@error": "./src/error"
-          },
-          extensions: [".js", ".ts"]
-
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src/"],
+        alias: {
+          "@modules": "./src/modules",
+          "@main": "./src/main",
+          "@error": "./src/error",
         },
-      ],
-      "babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
+        extensions: [".js", ".ts"],
+      },
     ],
-  };
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
