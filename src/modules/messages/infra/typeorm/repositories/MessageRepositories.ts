3e5d4b85949af7ffcb0153390effc7f582b4356c
DaniloSVJ <danilocotatoceu@gmail.com>
2024-08-23 17:54:08 -0300

    ajuste OldMessage

--- src/modules/messages/infra/typeorm/repositories/MessageRepositories.ts
+++ src/modules/messages/infra/typeorm/repositories/MessageRepositories.ts
@@ -33,6 +33,9 @@ class MessageRepository implements IMessageRepository {
             where: {
                 idMessage: { id },
 
+            },
+            order:{
+                createdAt:"DESC"
             }
         });
 
