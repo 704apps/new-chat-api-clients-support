136aafc3774a9c5422923a8117dcc33432729513
JoaoLucas8760 <canalluka8760@gmail.com>
2024-08-28 17:23:08 -0300

    fix cors errors

--- src/main/infra/http/routes/index.ts
+++ src/main/infra/http/routes/index.ts
@@ -1,17 +1,14 @@
-import "reflect-metadata"
+import "reflect-metadata";
 
-import {Router} from 'express'
+import { Router } from "express";
 
-import { chatRouter } from './chat.routes'
-import {messageRoutes} from './message.routes'
-import { autheticateRoutes } from "./authenticate.routes"
-import { noteRouter } from "./note.routes"
+import { chatRouter } from "./chat.routes";
+import { messageRoutes } from "./message.routes";
+import { autheticateRoutes } from "./authenticate.routes";
+import { noteRouter } from "./note.routes";
 
-const router = Router()
+const router = Router();
 
+router.use("/chat", messageRoutes, chatRouter, autheticateRoutes, noteRouter);
 
-router.use("/chat",messageRoutes,chatRouter,autheticateRoutes,noteRouter)
-
-
-
-export {router}
+export { router };
